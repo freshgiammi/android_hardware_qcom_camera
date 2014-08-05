1ef913f77bc0719f78a4cb0be63971d1de7c8926
Kamen Kirov <kkirov@codeaurora.org>
2014-08-05 16:58:44 +0300

    camera: jpeg: Remove compilation warnings
    1. Removes compilation warnings in  jpeg interface
    2. Set -Werror compilation flag
    
    Change-Id: Ide694135420e14578770d7d1dedf4cf731cb4f0b

--- QCamera2/stack/mm-jpeg-interface/Android.mk
+++ QCamera2/stack/mm-jpeg-interface/Android.mk
@@ -6,6 +6,7 @@ LOCAL_CLANG_CFLAGS += \
         -Wno-error=unused-variable
         
 LOCAL_CFLAGS+= -D_ANDROID_
+LOCAL_CFLAGS += -Werror
 
 LOCAL_C_INCLUDES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/media
