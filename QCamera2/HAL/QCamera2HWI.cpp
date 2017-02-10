17ea9abc199e66186d975bb90976676047238093
freshgiammi <rengucci.gianmarco@gmail.com>
2017-02-10 21:06:32 +0100

    Revert "QCamera2: Fix slow-shutter feature with CameraNext app"
    This reverts commit 48c019b7bc556fa2189d07abec527e5e5b76868d.
    
    Signed-off-by: freshgiammi <rengucci.gianmarco@gmail.com>

--- QCamera2/HAL/QCamera2HWI.cpp
+++ QCamera2/HAL/QCamera2HWI.cpp
@@ -810,21 +810,20 @@ char* QCamera2HardwareInterface::get_parameters(struct camera_device *device)
     if (rc == NO_ERROR) {
         hw->waitAPIResult(QCAMERA_SM_EVT_GET_PARAMS, &apiResult);
         ret = apiResult.params;
-
-            android::CameraParameters params;
-            params.unflatten(android::String8(hw->m_apiResult.params));
-
-            // Hide nv12-venus from userspace to prevent framework crash
-            const char *fmt = params.get("preview-format");
-            if (fmt && !strcmp(fmt, "nv12-venus")) {
+        
+        // Mask nv12-venus to userspace to prevent framework crash
+        if (hw->mParameters.getRecordingHintValue()) {
+            int width, height;
+            hw->mParameters.getVideoSize(&width, &height);
+            if ((width * height) >= (1280 * 720)) {
+                android::CameraParameters params;
+                params.unflatten(android::String8(hw->m_apiResult.params));
                 params.set("preview-format", "yuv420sp");
+                ret = strdup(params.flatten().string());
             }
-
-            // Set exposure-time-values param for CameraNext slow-shutter
-            params.set("exposure-time-values", "0");
-
-            ret = strdup(params.flatten().string());
-        }
+          }
+        if (ret == NULL)
+            ret = hw->m_apiResult.params;
     }
     hw->unlockAPI();
 
